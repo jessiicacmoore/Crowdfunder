f6a136ac3dd3f60484a20037abf8f92cf1201f00
Jessica Moore <jessica.cmoore1@gmail.com>
2019-06-07 14:04:50 -0400

    Commented out a few attributes

--- crowdfunder/models.py
+++ crowdfunder/models.py
@@ -13,7 +13,7 @@ class Project(models.Model):
     amount_funded = models.DecimalField(decimal_places=1, max_digits=4, default=0)
     number_of_backers = models.IntegerField()
     # category = models.CharField(max_length=255)
-    status_updates =  models.CharField(max_length=255)
+    # status_updates =  models.CharField(max_length=255)
 
     def __str__(self):
         return f'{self.title}'
