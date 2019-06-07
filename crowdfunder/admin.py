9aa9753118e6ce126976c8a633c87a691e8189df
Jessica Moore <jessica.cmoore1@gmail.com>
2019-06-07 13:51:00 -0400

    Registered all models with admin

---
+++ crowdfunder/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Project)
+admin.site.register(Reward)
+admin.site.register(Profile)
+admin.site.register(Comment)
+admin.site.register(Donation)
