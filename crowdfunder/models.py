7041918d431a3480f860789b9c009a66016f847e
Alexander G. Yang <spark8402@rogers.com>
2019-06-07 12:18:14 -0400

    models.py created

---
+++ crowdfunder/models.py
@@ -0,0 +1,43 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Project(models.Model):
+    owner = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    funding_goal = models.DecimalField(decimal_places=1, max_digits=4, default=0)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    amount_funded = models.DecimalField(decimal_places=1, max_digits=4, default=0)
+    number_of_backers = models.IntegerField()
+    category = models.CharField(max_length=255)
+    rewards = models.CharField(max_length=255)
+    status_updates =  models.CharField(max_length=255)
+    comments = models.TextField()
+    pictures = models.URLField()
+
+    def __str__(self):
+        return f'{self.title}'
+
+class Reward(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='rewards')
+
+class Profile(models.Model):
+    name = models.CharField(max_length=255)
+    owned_project = models.CharField(max_length=255)
+    funded_project = models.CharField(max_length=255)
+    commment = models.TextField()
+
+class Comment(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='comments')
+    message = models.TextField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='comments')
+
+class Donation(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='donations')
+    reward = models.ForeignKey(Reward, on_delete=models.CASCADE, related_name='donations')
+    donation_amount = models.DecimalField(decimal_places=1, max_digits=4, default=0)
+
